8bd2095d711bef0576c08b510a844ad8b024d2d2
Olegsander48 <prigodichaleks@gmail.com>
2024-11-30 18:15:58 +0300

    5.0.1. Сумма чисел. [#226416 #269276]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class CounterTest {
     @Test
-    public void whenSumFrom9To7Is0(){
+    public void whenSumFrom9To7Is0() {
         int start = 9;
         int end = 7;
         int expected = 0;
@@ -14,7 +14,7 @@ class CounterTest {
     }
 
     @Test
-    public void whenSumFromMinus5To15Is105(){
+    public void whenSumFromMinus5To15Is105() {
         int start = -5;
         int end = 15;
         int expected = 105;
@@ -22,10 +22,37 @@ class CounterTest {
     }
 
     @Test
-    public void whenSumFrom1To10Is55(){
+    public void whenSumFrom1To10Is55() {
         int start = 1;
         int end = 10;
         int expected = 55;
         assertThat(Counter.sum(start, end)).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom5To17Then66() {
+        int start = 5;
+        int finish = 17;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 66;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinus3To9Then18() {
+        int start = -3;
+        int finish = 9;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
     }
 }
