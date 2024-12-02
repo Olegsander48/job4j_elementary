88fd030b01e27bcf2e2a8d0e9dca98697727909b
Olegsander48 <prigodichaleks@gmail.com>
2024-12-02 20:43:26 +0300

    6.0.3. Заполнение массива. [#226300 #269817]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Size of surnames is " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Size of prices is " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Pavel";
+        names[1] = "Nikita";
+        names[2] = "Sasha";
+        names[3] = "Artyom";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
